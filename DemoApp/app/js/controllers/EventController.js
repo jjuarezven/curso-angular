fbc1eb0ade7ceedccffa8b323f14b7ba9bcc8a60
jjuarezven <josejuarez78@gmail.com>
2016-03-13 15:09:57 -0500

    Second commit

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -2,9 +2,14 @@
 
 eventsApp.controller('EventController',
 	function EventController($scope){
+		$scope.snippet = '<span style="color:red">HI THERE</span>';
+		$scope.boolValue = true;
+		$scope.buttonDisabled = true;
+		$scope.myStyle = {color:'red'};
+		$scope.sortOrder = 'name';
 		$scope.event = {
 			name: 'Angular Boot Camp',
-			date: '1/1/2016',
+			date: 1288323623006,
 			time: '10:30 am',
 			location:{
 				address: "Carrera 37, #39B sur 1 - 401",
@@ -19,13 +24,15 @@ eventsApp.controller('EventController',
 					duration: '1 hour',
 					level: 'Advanced',
 					abstract: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, voluptas rerum molestiae quidem molestias corporis dignissimos sed nam eligendi dolorem adipisci doloremque veritatis enim consequatur fugiat. Nesciunt, molestias aliquid sapiente.'
+					,upVoteCount: 0
 				},
-				{
+					{
 					name: 'Scopes for fun and profit',
 					creatorName: 'John Doe',
 					duration: '30 minutes',
 					level: 'Introductory',
 					abstract: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, voluptas rerum molestiae quidem molestias corporis dignissimos sed nam eligendi dolorem adipisci doloremque veritatis enim consequatur fugiat. Nesciunt, molestias aliquid sapiente.'
+					,upVoteCount: 0
 				},
 				{
 					name: 'Well behaved controllers',
@@ -33,8 +40,24 @@ eventsApp.controller('EventController',
 					duration: '2 houra',
 					level: 'Intermediate',
 					abstract: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, voluptas rerum molestiae quidem molestias corporis dignissimos sed nam eligendi dolorem adipisci doloremque veritatis enim consequatur fugiat. Nesciunt, molestias aliquid sapiente.'
+					,upVoteCount: 0
 				}
 			]
 		}
+		
+		$scope.upVoteSession = function(session){
+			session.upVoteCount++;
+		};
+		
+		$scope.downVoteSession = function(session){
+			if(session.upVoteCount > 0){
+				session.upVoteCount--;
+			}
+		};
+		
+		$scope.switchBoolValue = function(){
+			$scope.boolValue = !$scope.boolValue;
+			$scope.buttonDisabled = $scope.boolValue;
+		};
 	}
 );
