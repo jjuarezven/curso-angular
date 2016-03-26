c1cc18a57e4fdcd0497a0be7b0d011b07328fc0f
jjuarezven <josejuarez78@gmail.com>
2016-03-26 15:43:50 -0500

    Usando $http promises

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -1,16 +1,23 @@
 'use strict';
 
 eventsApp.controller('EventController',
-	function EventController($scope, eventData){
+	function EventController($scope, eventData, $log){
 		$scope.snippet = '<span style="color:red">HI THERE</span>';
 		$scope.boolValue = true;
 		$scope.buttonDisabled = true;
 		$scope.myStyle = {color:'red'};
 		$scope.sortOrder = 'name';
 		//$scope.event = eventData.event;
-		eventData.getEvent(function(event){
+		// usando callback
+		/*eventData.getEvent(function(event){
 			$scope.event = event;
-		});
+		});*/
+		// usando $http promises
+		eventData.getEvent()
+		.success(function(event){ $scope.event = event; })
+		.error(function(data, status, headers, config){
+				$log.warn(data, status, headers(), config);
+			});
 		
 		$scope.upVoteSession = function(session){
 			session.upVoteCount++;
--- DemoApp/app/js/services/EventData.js
+++ DemoApp/app/js/services/EventData.js
@@ -1,16 +1,25 @@
 'use strict';
 
-eventsApp.factory('eventData', function($http, $log){
+// usando una funcion callback
+/*eventsApp.factory('eventData', function($http, $log){
 	return {
 		getEvent: function(successcb) {			
 			$http({method: 'GET', url: '/data/event/2'})
 			.success(function(data, status, headers, config){
-				debugger;
 				successcb(data);
 			})
 			.error(function(data, status, headers, config){
 				$log.warn(data, status, headers(), config);
 			})
+		}
+	};
+});*/
+
+// usando $http promise
+eventsApp.factory('eventData', function($http){
+	return {
+		getEvent: function() {			
+			return $http({method: 'GET', url: '/data/event/2'});			
 		}
 	};
 });
