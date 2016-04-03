ea300399e144d065a32def3bd9fca142cb8b7f3c
jjuarezven <josejuarez78@gmail.com>
2016-04-03 13:28:00 -0500

    Integrating js into VS solution

---
+++ DemoApp/app/AppData/AppData/Scripts/controllers/EventController.js
@@ -0,0 +1,51 @@
+'use strict';
+
+eventsApp.controller('EventController',
+	function EventController($scope, eventData, $log, $anchorScroll){
+		$scope.snippet = '<span style="color:red">HI THERE</span>';
+		$scope.boolValue = true;
+		$scope.buttonDisabled = true;
+		$scope.myStyle = {color:'red'};
+		$scope.sortOrder = 'name';
+		//$scope.event = eventData.event;
+		
+		// usando callback
+		/*eventData.getEvent(function(event){
+			$scope.event = event;
+		});*/
+		
+		// usando $http promises
+		/*eventData.getEvent()
+		.success(function(event){ $scope.event = event; })
+		.error(function(data, status, headers, config){
+				$log.warn(data, status, headers(), config);
+			});*/
+		
+		// usando $resource
+		//$scope.event = eventData.getEvent();
+		eventData.getEvent()
+			.$promise
+			.then(function(event) {$scope.event = event;})
+			.catch(function(response) {console.log(response);});
+		
+		
+		$scope.upVoteSession = function(session){
+			session.upVoteCount++;
+		};
+		
+		$scope.downVoteSession = function(session){
+			if(session.upVoteCount > 0){
+				session.upVoteCount--;
+			}
+		};
+		
+		$scope.switchBoolValue = function(){
+			$scope.boolValue = !$scope.boolValue;
+			$scope.buttonDisabled = $scope.boolValue;
+		};
+		
+		$scope.scrollToSession = function() {
+			$anchorScroll();
+		}
+	}
+);
---
+++ DemoApp/app/AppData/AppData/Scripts/services/EventData.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// usando una funcion callback
+/*eventsApp.factory('eventData', function($http, $log){
+	return {
+		getEvent: function(successcb) {			
+			$http({method: 'GET', url: '/data/event/2'})
+			.success(function(data, status, headers, config){
+				successcb(data);
+			})
+			.error(function(data, status, headers, config){
+				$log.warn(data, status, headers(), config);
+			})
+		}
+	};
+});*/
+
+// usando $http promise
+/*eventsApp.factory('eventData', function($http){
+	return {
+		getEvent: function() {			
+			return $http({method: 'GET', url: '/data/event/2'});			
+		}
+	};
+});*/
+
+// usando $resource (se asume que el servicio es REST)
+eventsApp.factory('eventData', function($resource){
+	var resource = $resource('/data/event/:id', {id:'@id'});
+	return {
+		getEvent: function() {			
+			return resource.get({id:2});			
+		},
+		save: function(event){
+			event.id = 88;
+			return resource.save(event);
+		}
+	};
+});
