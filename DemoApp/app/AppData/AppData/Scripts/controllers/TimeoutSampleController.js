2a7890e1bf971efab2162277aec91fa00267c0f4
jjuarezven <josejuarez78@gmail.com>
2016-04-10 16:58:20 -0500

    Using $timeout service

---
+++ DemoApp/app/AppData/AppData/Scripts/controllers/TimeoutSampleController.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+eventsApp.controller('TimeoutSampleController',
+	function CacheSampleController($scope, $timeout) {
+
+		
+		var promise = $timeout(function () {
+			$scope.name = 'Jose Juarez';
+		}, 3000);
+
+		// el servicio $timeout es necesario para que la funcion setTimeout este dentro del contexto de angular, si ponemos aejecutar la funcion de abajo
+		// vemos que el nombre no aparece despues de los 3 segundos porque setTimeout se ejecuta en otro thread diferente al de angular, vemos que se 
+		// acomoda cuando se reevaluan los bindings al escribir en la caja de texto.
+		//setTimeout(function () {
+		//	$scope.name = 'Jose Juarez';
+		//}, 3000);
+
+		$scope.cancel = function () {
+			$timeout.cancel(promise);
+		}
+	}
+);
