9375efe4a1c2bc12b756d034fa1609401f9f22b7
JJuarez <jose.juarez@yuxipacific.com>
2016-07-11 17:48:02 -0500

    Minor fixes for compile service

--- DemoApp/app/AppData/AppData/Scripts/controllers/CompileSampleController.js
+++ DemoApp/app/AppData/AppData/Scripts/controllers/CompileSampleController.js
@@ -3,7 +3,7 @@
 eventsApp.controller('CompileSampleController',
 	function CompileSampleController($scope, $compile, $parse) {
 		$scope.appendDivToElement = function (markup) {
-			return $compile(markup) ($scope).appendTo(angular.element("#aqui"));
+			return $compile(markup)($scope).appendTo(angular.element("#aqui"));
 		}
 
 		// uso del servicio parse
--- DemoApp/app/AppData/AppData/Scripts/controllers/LocalSampleController.js
+++ DemoApp/app/AppData/AppData/Scripts/controllers/LocalSampleController.js
@@ -4,5 +4,6 @@ eventsApp.controller('LocalSampleController',
 	function CacheSampleController($scope, $locale) {
 		$scope.myDate = Date.now();
 		$scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
+		console.log($locale);
 	}
 );
