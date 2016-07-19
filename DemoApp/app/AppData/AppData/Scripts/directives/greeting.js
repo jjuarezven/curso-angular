05d6eb65ca8b7fb548abba3bd87615602c1be463
JJuarez <jose.juarez@yuxipacific.com>
2016-07-19 10:56:42 -0500

    Declaracion de controles dentro de directivas

---
+++ DemoApp/app/AppData/AppData/Scripts/directives/greeting.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+
+eventsApp.directive('greeting', function () {
+	return {
+		restrict: "E",
+		replace: true,
+		template: '<button class="btn" ng-click="sayHello()">Say Hello</button>',
+		// declaracion inline
+		//controller: function ($scope) {
+		//	$scope.sayHello = function () {
+		//		alert("Hello");
+		//	}
+		//}
+
+		// usando una declaracion externa
+		//controller: "GreetingController"
+
+		// el controller lo define un atributo en el html
+		controller: "@",
+		name: "ctrl"
+	};
+});
+
+
+// puede estar definido en un archivo aparte
+eventsApp.controller("GreetingController",
+	function GreetingController($scope) {
+		$scope.sayHello = function () {
+			alert("Hello");
+		}
+	});
