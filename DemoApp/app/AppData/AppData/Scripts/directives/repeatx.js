990d6bf51cc2f1e33ab21a249749466eba413255
JJuarez <jose.juarez@yuxipacific.com>
2016-07-22 18:02:32 -0500

    Compile por terminar

---
+++ DemoApp/app/AppData/AppData/Scripts/directives/repeatx.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+eventsApp.directive('repeatX', function ($compile) {
+	return {
+		link: function (scope, element, attrs, controller) {
+			for (var i = 0; i < Number(attrs.repeatX) - 1; i++) {
+				element.after($compile(element.clone().attr("repeat-x", 0)) (scope));
+			}
+		}
+	};
+});
