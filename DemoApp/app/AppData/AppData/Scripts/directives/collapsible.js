86019563d3d2209f1bfbf13d65aad0e8d58bc5c0
JJuarez <jose.juarez@yuxipacific.com>
2016-07-22 17:12:10 -0500

    Terminando transclusion

--- DemoApp/app/AppData/AppData/Scripts/directives/collapsible.js
+++ DemoApp/app/AppData/AppData/Scripts/directives/collapsible.js
@@ -4,9 +4,16 @@ eventsApp.directive('collapsible', function () {
 	return {
 		restrict: "E",
 		replace: true,
-		template: "<div><h4 class='well-title'>{{title}}</h4></div>",
+		transclude: true,
+		template: "<div><h4 class='well-title' ng-click='toggleVisibility()'>{{title}}</h4><div ng-show='visible' ng-transclude></div></div>",
 		scope: {
 			title: "@"
-		}
+		},
+		controller: function ($scope) {
+			$scope.visible = false;
+			$scope.toggleVisibility = function () {
+				$scope.visible = !$scope.visible;
+			}
+		} 
 	};
 });
